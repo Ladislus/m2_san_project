79bdd4a6ba4b33764e52b2e4ee28a3b158fc74ec
Ladislus <walcak.ladislas@gmail.com>
2021-11-06 00:14:13 +0100

    [MOD] Removed varargs for Flag to prevent conversion from list

--- tools/parser.py
+++ tools/parser.py
@@ -6,7 +6,7 @@ def parse() -> (str, str, int):
 
     parser.add_argument('APKFile', type=str, help='Path to the APK file to analyse')
     parser.add_argument('Class', type=str, help='Name of the class to analyse')
-    parser.add_argument('Flag', type=int, help='Type of analyse', choices=[1, 2, 3], nargs='?', default=1)
+    parser.add_argument('Flag', type=int, help='Type of analyse', choices=[1, 2, 3])
 
     args = parser.parse_args()
 
