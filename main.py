dfdeb40b460d23e0e082cfbc6bdc778e95ed3dd5
Ladislus <walcak.ladislas@gmail.com>
2021-11-06 00:14:53 +0100

    [MOD] Updated annotation typing with APKKeys

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from tools import parse, extract
+from typing import Any
+
+from tools import parse, extract, APKKeys
 
 if __name__ == '__main__':
     APKPath, ClassName, Flag = parse()
-    infos: dict = extract(APKPath)
+    infos: dict[APKKeys, Any] = extract(APKPath)
 
     print(infos)
