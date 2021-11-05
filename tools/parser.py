490fd9a16b7761e6f4ccc7d659072d4872e7a59a
Ladislus <walcak.ladislas@gmail.com>
2021-11-05 15:49:35 +0100

    [ADD] Added tools package

---
+++ tools/parser.py
@@ -0,0 +1,13 @@
+from argparse import ArgumentParser
+
+
+def parse() -> (str, str, int):
+    parser: ArgumentParser = ArgumentParser()
+
+    parser.add_argument('APKFile', type=str, help='Path to the APK file to analyse')
+    parser.add_argument('Class', type=str, help='Name of the class to analyse')
+    parser.add_argument('Flag', type=int, help='Type of analyse')
+
+    args = parser.parse_args()
+
+    return args.APKFile, args.Class, args.Flag
