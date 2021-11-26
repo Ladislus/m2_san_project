b8dcca47afd7d99788e9fa9d7d18faedf4a2ecd7
Ladislus <walcak.ladislas@gmail.com>
2021-11-26 16:16:57 +0100

    [ADD] Added optional parameter for input file for the third analysis

--- tools/exceptions.py
+++ tools/exceptions.py
@@ -7,3 +7,4 @@ class ExitCode(Enum):
     EXIT_SUCCESS = 0
     CLASS_NOT_FOUND = auto()
     MULTIPLE_CLASSES_FOUND = auto()
+    NO_INPUT_FILE_GIVEN = auto()
--- tools/parser.py
+++ tools/parser.py
@@ -1,13 +1,14 @@
 from argparse import ArgumentParser
 
 
-def parse() -> (str, str, int):
+def parse() -> (str, str, int, str | None):
     parser: ArgumentParser = ArgumentParser()
 
     parser.add_argument('APKFile', type=str, help='Path to the APK file to analyse')
     parser.add_argument('Class', type=str, help='Name of the class to analyse')
     parser.add_argument('Flag', type=int, help='Type of analyse', choices=[1, 2, 3])
+    parser.add_argument('-i', '--input', dest='File', type=str, help='Path to the input file (Analyse 3)')
 
     args = parser.parse_args()
 
-    return args.APKFile, args.Class, args.Flag
+    return args.APKFile, args.Class, args.Flag, args.File
